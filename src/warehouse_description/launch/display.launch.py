adc40a4199faf61fa52ee6bff76fbdf8aceb9cd3
unknown <1276967682@qq.com>
2026-09-22 10:46:51 +0800

    阶段2：加激光雷达与IMU插件，删除与驱动插件冲突的静态tf，移除不支持的odometry_source

--- src/warehouse_description/launch/display.launch.py
+++ src/warehouse_description/launch/display.launch.py
@@ -35,11 +35,4 @@ def generate_launch_description():
             name="map_to_odom",
             arguments=["0", "0", "0", "0", "0", "0", "map", "odom"],
         ),
-
-        Node(
-            package="tf2_ros",
-            executable="static_transform_publisher",
-            name="odom_to_base_link_test",
-            arguments=["0", "0", "0", "0", "0", "0", "odom", "base_link"],
-        ),
     ])
